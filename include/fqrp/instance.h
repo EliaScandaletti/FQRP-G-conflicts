fc50619040641f6623e7de1b4bb7de4d9fbf7e6c
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-05-30 16:58:06 +0200

    FIx instance destructor

--- include/fqrp/instance.h
+++ include/fqrp/instance.h
@@ -11,15 +11,13 @@ class Instance {
   const vehicle_t size;
   const vehicle_t *const permutation;
 
-protected:
-  Instance(vehicle_t size);
-
 public:
   Instance(std::vector<vehicle_t> perm);
   Instance(const vehicle_t *const perm, vehicle_t size);
+  ~Instance();
 
   vehicle_t getSize() const;
-  virtual vehicle_t sigma(vehicle_t vehicle) const;
+  vehicle_t sigma(vehicle_t vehicle) const;
 };
 
 } // namespace fqrp
--- src/instance.cpp
+++ src/instance.cpp
@@ -5,8 +5,6 @@
 
 using namespace fqrp;
 
-Instance::Instance(vehicle_t size) : size(size), permutation(nullptr) {}
-
 Instance::Instance(std::vector<vehicle_t> perm)
     : size(perm.size()), permutation(new vehicle_t[perm.size()]) {
   std::copy(perm.begin(), perm.end(), this->permutation);
@@ -16,6 +14,8 @@ Instance::Instance(const vehicle_t *const perm, vehicle_t size)
     : size(size), permutation(new vehicle_t[size]) {
   std::copy(perm, perm + size, permutation);
 }
+
+Instance::~Instance() { delete[] permutation; }
 
 vehicle_t Instance::sigma(vehicle_t vehicle) const {
   return permutation[vehicle];
