8e3aabb0897de9be7c7384356d8b7e9cce98cf19
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-05-31 18:49:08 +0200

    Alg2

--- include/fqrp/algorithms.h
+++ include/fqrp/algorithms.h
@@ -1,6 +1,8 @@
 #ifndef FQRP_ALGORITHMS_H
 #define FQRP_ALGORITHMS_H
 
+#include "aggregator.h"
+#include "generator.h"
 #include "instance.h"
 #include "types.h"
 
@@ -8,8 +10,26 @@ namespace fqrp {
 
 conflictCount getConflictCount(const Instance &instance);
 
-
+template <typename result_t>
+result_t getEstimatedConflictCount(vehicle_t size, count_t maxSample,
+                                   Generator *, Aggregator<result_t> *);
 
 } // namespace fqrp
+
+template <typename result_t>
+result_t fqrp::getEstimatedConflictCount(count_t maxSample,
+                                         Generator *generator,
+                                         Aggregator<result_t> *aggregator) {
+
+  count_t n = 0;
+
+  while (n < maxSample && !generator->finished()) {
+    Instance inst = generator->next();
+    aggregator->aggregate(getConflictCount(inst));
+    n += 1;
+  }
+
+  return aggregator->result();
+}
 
 #endif
