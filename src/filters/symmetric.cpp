edb30162325d79ce3d0649676d10d84b036eced5
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-06-16 20:50:37 +0200

    fix symmetric

--- src/filters/symmetric.cpp
+++ src/filters/symmetric.cpp
@@ -1,5 +1,5 @@
 #include <fqrp/filters/symmetric.h>
 
 bool fqrp::filters::ExcludeSymmetric::operator()(const Instance &instance) {
-  return instance.sigma(1) <= instance.sigma(instance.size());
+  return instance.sigma(1) >= instance.sigma(instance.size());
 }
