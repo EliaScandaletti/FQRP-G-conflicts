75240edde9c61ddd430626848bdd36ac93a3bb5b
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-06-17 00:04:55 +0200

    run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+cd src;
+
+make;
+
+./progs/exh_filt_gen_main 1 15 > ~/exh_filt_gen.out 2> ~/exh_filt_gen.err &
+
+./progs/exh_gen_main 1 15 > ~/exh_gen.out 2> ~/exh_gen.err &
+
+./progs/int_filt_gen_main 1 50 100000000 > ~/int_filt_gen.out 2> ~/int_filt_gen.err &
+
+./progs/rand_filt_gen_main 1 50 100000000 > ~/rand_filt_gen.out 2> ~/rand_filt_gen.err &
