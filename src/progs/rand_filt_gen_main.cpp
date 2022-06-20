9e0e2e81c2db0c579f7a0e9dfa1164d18377c967
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-06-20 23:46:04 +0200

    Added loggers in rand_filt_gen_main

--- src/progs/rand_filt_gen_main.cpp
+++ src/progs/rand_filt_gen_main.cpp
@@ -1,5 +1,7 @@
-#include <core/algorithms.h>
+#include <cmath>
+#include <fstream>
 
+#include <core/algorithms.h>
 #include <fqrp/aggregators/distribution_aggregator.h>
 #include <fqrp/counters/conflictCounter.h>
 #include <fqrp/filters/not.h>
@@ -8,11 +10,13 @@
 #include <fqrp/generators/randomGenerator.h>
 #include <fqrp/types.h>
 
+using fqrp::conflictCount;
 using fqrp::Instance;
 using fqrp::vehicle_t;
 using fqrp::aggregators::dist_count;
 using fqrp::aggregators::DistAggregator;
 using fqrp::counters::conflictCounter;
+using fqrp::counters::logger_t;
 using fqrp::filters::IsPartitioned;
 using fqrp::filters::Not;
 using fqrp::generators::FilteredGenerator;
@@ -42,11 +46,56 @@ int main(int argc, char const *argv[]) {
     cerr << "Invalid input: conversion error" << endl;
   }
 
+  std::ofstream nonMixedTreeOut("benchmark/non_mixed_tree.ins.dat",
+                                std::ios_base::app);
+  if (!nonMixedTreeOut) {
+    cerr << "Unable to create file benchmark/non_mixed_tree.ins.dat" << endl;
+    cerr << "Check if the directory exists" << endl;
+    throw "Unable to create file benchmark/non_mixed_tree.ins.dat";
+  }
+  logger_t nonMixedTreeLogger(
+      [](const Instance &instance, const conflictCount &count) {
+        (void)(instance); // remove unused parameter warning
+        return !count.mixed_forest_info.is_a_forest;
+      },
+      nonMixedTreeOut);
+
+  std::ofstream longCChainOut("benchmark/long_C_chain.ins.dat",
+                              std::ios_base::app);
+  if (!longCChainOut) {
+    cerr << "Unable to create file benchmark/long_C_chain.ins.dat" << endl;
+    cerr << "Check if the directory exists" << endl;
+    throw "Unable to create file benchmark/long_C_chain.ins.dat";
+  }
+  logger_t longCChainLogger(
+      [](const Instance &instance, const conflictCount &count) {
+        vehicle_t n = instance.size();
+        size_t theoretical_min = n >= 3 ? 1 + std::floor((n - 1) / 4) : 0;
+        return count.c_graph_info.max_length >=
+               std::floor(0.8 * theoretical_min);
+      },
+      longCChainOut);
+
+  std::ofstream mixedChainOut("benchmark/mixed_chain.ins.dat",
+                              std::ios_base::app);
+  if (!mixedChainOut) {
+    cerr << "Unable to create file benchmark/mixed_chain.ins.dat" << endl;
+    cerr << "Check if the directory exists" << endl;
+    throw "Unable to create file benchmark/mixed_chain.ins.dat";
+  }
+  logger_t mixedChainLogger(
+      [](const Instance &instance, const conflictCount &count) {
+        (void)(instance); // remove unused parameter warning
+        vehicle_t k = 3;
+        return count.mixed_forest_info.max_tree_size >= k;
+      },
+      mixedChainOut);
+
   for (vehicle_t size = min_size; size <= max_size; size++) {
     RandomGenerator g(size);
     Not<IsPartitioned> part_filter;
     FilteredGenerator<Instance> fg(g, part_filter);
-    conflictCounter c;
+    conflictCounter c({nonMixedTreeLogger, longCChainLogger, mixedChainLogger});
     DistAggregator agg;
     dist_count res = core::getEstimatedCount(limit, fg, c, agg);
 
