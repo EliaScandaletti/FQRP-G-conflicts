2b1358a45cf137680869e661f0fadf3810ba9a14
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-06-23 13:57:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ make;
 
 ./progs/int_filt_gen_main 1 50 100000000 > ~/int_filt_gen.out 2> ~/int_filt_gen.err &
 
-{./progs/rand_filt_gen_main 1 15 100000000; ./progs/rand_filt_log_main 16 50 100000000} > ~/rand_filt_gen.out 2> ~/rand_filt_gen.err &
+{ ./progs/rand_filt_gen_main 1 15 100000000 && ./progs/rand_filt_log_main 16 50 100000000; } > ~/rand_filt_gen.out 2> ~/rand_filt_gen.err &
